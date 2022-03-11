4ce72e9c229af81d47b6f9662574791b180630ab
joseffe10 <joseffe_oliveira@whirlpool.com>
2022-03-11 16:42:57 -0300

    Primeiro app

---
+++ app00_hello/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: widgetHome(),
+    );
+  }
+}
+
+class widgetHome extends StatelessWidget {
+  const widgetHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App Hello"),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Text(
+            "Olá Turma",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              decoration: TextDecoration.underline,
+              //decoration: TextDecoration.overline,
+              decorationColor: Colors.red,
+              decorationStyle: TextDecorationStyle.dashed,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
